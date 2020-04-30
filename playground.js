d8cb0e2ee2ece0a940b16d5296b59be871794691
shubham <shubham.chauhan860@gmail.com>
2020-04-30 23:51:37 +0530

    update

---
+++ playground.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+console.log(path.join('../../hello','../things'));
+//const file = fs.readFileSync('./fundamental.js',{encoding : 'utf-8'}).toString();
+//console.log(file);
+//fs.writeFileSync('./fundamental.js','const data  =  "smug"; console.log(data);');
+
